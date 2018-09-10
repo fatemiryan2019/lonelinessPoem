fe76abef92a0be77535e392efb7ad7c99ed12cd1
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2018-09-10 08:25:01 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.print("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
+    }
+}
+//l(a
+//
+//le
+//af
+//fa
+//
+//ll
+//
+//s)
+//one
+//l
+//
+//iness
